8fc094064157d5bf931aa6031de6621e35b06b8d
NikhilPal79 <nikhilchpr7@gmail.com>
2025-04-17 20:53:25 -0400

    added in map

---
+++ src/Map/Employee.java
@@ -0,0 +1,14 @@
+package Map;
+
+public class Employee {
+
+
+    /// Classes that Implement Map
+    /// The following are the classes that implement a Map to use the functionalities of a Map -
+    ///
+    /// HashMap
+    /// EnumMap
+    /// LinkedHashMap
+    /// WeakHashMap
+    /// TreeMap
+}
